20de9708cd7fa1ce6f4fd0ffe4709e5fe162fe0b
xiaoyu <yuriyshea@163.com>
2021-10-07 19:55:48 +0800

    Calculator

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -69,16 +69,23 @@ class _CalculatorState extends State<Calculator> {
                   child: TextField(
                 keyboardType: TextInputType.number,
                 onChanged: (value) {
-                  int result = _convertNumber(value);
-                  if (result == null) {
-                    this.setState(() {
-                      _textFieldAError = "请输入合法的$_typeName数";
-                    });
-                  } else {
+                  if (value == null || value.isEmpty) {
                     this.setState(() {
                       _textFieldAError = null;
-                      _a = result;
+                      _a = 0;
                     });
+                  } else {
+                    int result = _convertNumber(value);
+                    if (result == null) {
+                      this.setState(() {
+                        _textFieldAError = "请输入合法的$_typeName数";
+                      });
+                    } else {
+                      this.setState(() {
+                        _textFieldAError = null;
+                        _a = result;
+                      });
+                    }
                   }
                 },
                 decoration: InputDecoration(
@@ -86,6 +93,7 @@ class _CalculatorState extends State<Calculator> {
               ))
             ],
           ),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(
@@ -95,16 +103,23 @@ class _CalculatorState extends State<Calculator> {
                   child: TextField(
                 keyboardType: TextInputType.number,
                 onChanged: (value) {
-                  int result = _convertNumber(value);
-                  if (result == null) {
-                    this.setState(() {
-                      _textFieldBError = "请输入合法的$_typeName数";
-                    });
-                  } else {
+                  if (value == null || value.isEmpty) {
                     this.setState(() {
                       _textFieldBError = null;
-                      _b = result;
+                      _b = 0;
                     });
+                  } else {
+                    int result = _convertNumber(value);
+                    if (result == null) {
+                      this.setState(() {
+                        _textFieldBError = "请输入合法的$_typeName数";
+                      });
+                    } else {
+                      this.setState(() {
+                        _textFieldBError = null;
+                        _b = result;
+                      });
+                    }
                   }
                 },
                 decoration: InputDecoration(
@@ -112,6 +127,7 @@ class _CalculatorState extends State<Calculator> {
               ))
             ],
           ),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(
@@ -168,50 +184,60 @@ class _CalculatorState extends State<Calculator> {
               )),
             ],
           ),
+          SizedBox(height: 20),
           ElevatedButton(
-              child: Text("计算"),
-              onPressed: () {
-                if (_textFieldAError != null) {
-                  Fluttertoast.showToast(msg: "请输入合法的$_typeName数 A");
-                  return;
-                }
-                if (_textFieldBError != null) {
-                  Fluttertoast.showToast(msg: "请输入合法的$_typeName数 B");
-                  return;
-                }
-                this.setState(() {
-                  if (_operate == operateAdd) {
-                    _result = _a + _b;
-                  } else if (_operate == operateSubtract) {
-                    _result = _a - _b;
-                  } else if (_operate == operateMultiply) {
-                    _result = (_a * _b);
-                  } else if (_operate == operateDivide) {
-                    if (_b != 0) {
-                      _result = _a ~/ _b;
-                    }
+            child: Text("开始计算"),
+            onPressed: () {
+              if (_textFieldAError != null) {
+                Fluttertoast.showToast(msg: "请输入合法的$_typeName数 A");
+                return;
+              }
+              if (_textFieldBError != null) {
+                Fluttertoast.showToast(msg: "请输入合法的$_typeName数 B");
+                return;
+              }
+              this.setState(() {
+                if (_operate == operateAdd) {
+                  _result = _a + _b;
+                } else if (_operate == operateSubtract) {
+                  _result = _a - _b;
+                } else if (_operate == operateMultiply) {
+                  _result = (_a * _b);
+                } else if (_operate == operateDivide) {
+                  if (_b != 0) {
+                    _result = _a ~/ _b;
                   }
-                });
-              }),
-          Text("计算结果："),
+                }
+              });
+            },
+            style: ButtonStyle(
+                padding: MaterialStateProperty.all(
+                    EdgeInsets.fromLTRB(100, 20, 100, 20))),
+          ),
+          SizedBox(height: 50),
+          Row(children: [Text("计算结果：", textAlign: TextAlign.left)]),
+          SizedBox(height: 50),
           Row(
             children: [
               Expanded(child: Text("二进制", textAlign: TextAlign.center)),
               Expanded(child: Text(_result.toRadixString(2)))
             ],
           ),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(child: Text("八进制", textAlign: TextAlign.center)),
               Expanded(child: Text(_result.toRadixString(8)))
             ],
           ),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(child: Text("十进制", textAlign: TextAlign.center)),
               Expanded(child: Text(_result.toString()))
             ],
           ),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(child: Text("十六进制", textAlign: TextAlign.center)),
